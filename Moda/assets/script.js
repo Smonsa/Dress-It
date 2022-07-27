c9ae5fc54cb0b12480fea01b318b74b64e610223
Smonsa <82639896+Smonsa@users.noreply.github.com>
2022-07-27 02:40:39 -0400

    Add files via upload

---
+++ Moda/assets/script.js
@@ -0,0 +1,13 @@
+var titulo= document.getElementById('titulo');
+
+
+
+function move(){
+	titulo.classList.add('animate__animated',  'animate__bounce', 'animate__faster');
+
+}
+
+titulo.onclick= move;
+
+
+
